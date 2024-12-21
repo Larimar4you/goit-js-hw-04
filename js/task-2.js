812723c0cd4e09ca81170098b3e7df3f86fbc1d3
Larimar4you <Zajanezlaja@gmail.com>
2024-12-21 18:45:26 +0100

    git push

--- js/task-2.js
+++ js/task-2.js
@@ -9,14 +9,11 @@
 */
 
 `use strict`;
-  function makeArray(firstArray, secondArray, maxLength) {
-    const combinedArray = firstArray.concat(secondArray);
-    if (combinedArray.length > maxLength) {
-      return combinedArray.slice(0, maxLength);
-    } else {
-      return combinedArray;
-    }
-  }
+function calcAverageCalories(days) {
+  if (days.length === 0) return 0; 
+  const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+  return totalCalories / days.length;
+}
 
 /* Перевiрка функцii */
 console.log(
